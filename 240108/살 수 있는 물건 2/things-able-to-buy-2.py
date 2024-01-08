dea78d6e5fff6d3ed4209b5596035ce91dbefd8e
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-01-08 21:13:51 +0900

    Title: 살 수 있는 물건 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240108/살 수 있는 물건 2/things-able-to-buy-2.py
+++ 240108/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -2,9 +2,9 @@ a = int(input())
 
 if a>=3000:
     print("book")
-if a>= 1000:
+elif a>= 1000:
     print("mask")
-if a>=500:
-    print("500")
+elif a>=500 :
+    print("pen")
 else:
     print("no")
