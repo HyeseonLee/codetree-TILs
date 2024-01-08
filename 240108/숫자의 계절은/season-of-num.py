ae91fdfa943491e6689657209513d6787d4665a6
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-01-08 22:05:13 +0900

    Title: 숫자의 계절은, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240108/숫자의 계절은/season-of-num.py
+++ 240108/숫자의 계절은/season-of-num.py
@@ -4,7 +4,7 @@ if a>=3 or a<=5:
     print("Spring")
 elif a>=6 or a<=8:
     print("Summer")
-elif  a>=9 or a<=11:
+elif a>=9 or a<=11:
     print("Fall")
-elif  a==12 or a<=2:
+elif a==12 or a<=2:
     print("Winter")
