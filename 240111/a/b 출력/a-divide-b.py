8a6444573b7320dd65684c091b9f70f16e936af5
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-01-11 18:04:32 +0900

    Title: a/b 출력, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/a/b 출력/a-divide-b.py
+++ 240111/a/b 출력/a-divide-b.py
@@ -2,13 +2,7 @@ a, b = map(int, input().split())
 
 print(a//b, end=".")
 
-rest = a
-
+rest = a%b
 for _ in range(20):
-    if a//b > 0:
-        print((rest-b)*10 // b, end="")
-        rest = (rest-b)*10 % b
-
-    else:
-        print((rest*10) // b, end="")
-        rest = rest*10 % b
+    print((rest*10) // b, end="")
+    rest = rest*10 % b
