3ee7c75df76cb92da60ba1ee57fb507125ae8f95
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-01-17 19:44:38 +0900

    Title: 특정 조건 배수의 합, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240117/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 240117/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,8 +1,12 @@
 a,b = map(int, input().split())
 c=0
-
-for i in range(a,b+1):
-    if i%5==0:
-        c+=i
+if a>b:
+    for i in range(a,b+1):
+        if i%5==0:
+            c+=i
+else:
+    for i in range(b,a+1):
+        if i%5==0:
+            c+=i
 
 print(c)
