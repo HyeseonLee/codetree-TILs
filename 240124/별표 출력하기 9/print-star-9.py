77d52259b6b359b31fdea11250972879617c1ae5
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-01-24 18:10:20 +0900

    Title: 별표 출력하기 9, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240124/별표 출력하기 9/print-star-9.py
+++ 240124/별표 출력하기 9/print-star-9.py
@@ -3,6 +3,6 @@ n = int(input())
 for i in range(n):
     for k in range(n-i+1):
         print(" ",end=" ")
-    for j in range(2*n+1):
+    for j in range(2*i+1):
         print("*",end=" ")
     print()
