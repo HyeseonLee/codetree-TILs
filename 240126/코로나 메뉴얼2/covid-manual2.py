c88e489367fdbbb4ce8e59605d17046b2f5993fc
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-01-26 20:40:08 +0900

    Title: 코로나 메뉴얼2, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240126/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,23 @@
+# 코로 ~ 나 메뉴 ~ 얼
+
+
+l = [0,0,0,0]
+
+for _ in range(3):
+    e = 0
+    s,t = input().split()
+
+    if s=='Y' and int(t)>=37:
+        l[0]+=1
+    elif s=='Y' and int(t)<37:
+        l[2] += 1
+    elif s=='N' and int(t)>=37:
+        l[1] += 1
+    elif s=='N' and int(t)<37:
+        l[3] += 1
+
+for elem in l:
+    print(elem, end=" ")
+
+if l[1]>=2:
+    print('E')
