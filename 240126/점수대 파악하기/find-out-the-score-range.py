ca2c365bc31d951c998117bd3b902a568a0b21b1
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-01-26 20:21:17 +0900

    Title: 점수대 파악하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/점수대 파악하기/find-out-the-score-range.py
+++ 240126/점수대 파악하기/find-out-the-score-range.py
@@ -1,10 +1,12 @@
 arr = list(map(int, input().split())) 
 
-c_arr = [0 for _ in range(len(arr)+1)]
+# 10점부터 100점까지 10개 필요 ! 1 ~ 10 까지 필요 !
+c_arr = [0 for _ in range(11)]
+
 for elem in arr:
     if elem == 0:
         break
     c_arr[elem//10] += 1
 
-for i in range(100,0,-10):
-    print(f"{i} - {c_arr[i//10]}")
+for i in range(10,0,-1):
+    print(f"{i*10} - {c_arr[i]}")
