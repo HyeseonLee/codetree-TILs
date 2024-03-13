1040c3ee5644a15a90e6f438c9ce7aa63d74d8ee
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-03-13 18:32:30 +0900

    Title: 나눗셈의 나머지, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,19 @@
+# a를 b로 나눈 몫을 또 a로 나눈다. (a가 1 이하가 되기 전까지)
+# 나머지가 나온 횟수를 저장한다.
+# 나머지가 나온 횟수들의 제곱을 더해 출력한다.
+
+
+a,b = map(int, input().split())
+c=0
+res=0
+nam_arr=[0 for _ in range(11)]
+
+while(a>1):
+    a = a//b
+    c = a%b
+    nam_arr[c] += 1
+
+for item in nam_arr:
+    res += (item**2)
+
+print(res)
