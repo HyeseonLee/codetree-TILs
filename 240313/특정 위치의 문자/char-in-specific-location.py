74cea996f58d52c2c79d1dc4a325df73b6f726ab
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-03-13 19:06:51 +0900

    Title: 특정 위치의 문자, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/특정 위치의 문자/char-in-specific-location.py
+++ 240313/특정 위치의 문자/char-in-specific-location.py
@@ -4,9 +4,14 @@ char_arr = ['L', 'E', 'B', 'R', 'O', 'S']
 
 a = input()
 
-if a in char_arr:
-    for i, value in enumerate(char_arr):
-        if value==a:
-            print(i)
 if a not in char_arr:
     print('None')
+else:
+    # char_arr에 a가 있다는 가정이 성립했으니까
+    # index 위치를 알면 되네
+
+    # index 위치를 알아내는 방법 1
+    # for i, value in enumerate(char_arr):
+    #     if value==a:
+    #         print(i)  
+    print(char_arr.index(a))
