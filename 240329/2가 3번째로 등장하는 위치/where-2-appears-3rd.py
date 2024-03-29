c3799d1394466ba7a021bfb6e7410636f485b95a
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-03-29 19:00:26 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240329/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,11 @@
+num = int(input())
+arr = list(input().split())
+count = 0
+# 3번째로 등장할 때, 해당 index를 출력하는 것이다.
+for index, value in enumerate(arr):
+    if value == 2:
+        count += 1
+
+    if count==3:
+        print(index)
+        break
