4e69c5f73a60970fc05866bf4b1a821447f51318
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-04-05 21:16:28 +0900

    Title: 격자 반대로 채우기, Time: 224ms, Memory: 24MB, Status: Passed - CodeTree

--- 240405/격자 반대로 채우기/grid-anti-fill.py
+++ 240405/격자 반대로 채우기/grid-anti-fill.py
@@ -6,13 +6,16 @@ arr = [
 
 num =1
 for i in range(n-1, -1, -1):
-    if i%2 == 0:
-        for j in range(n):
-            arr[j][i] = num
-            num += 1
-    else:
+    # n이 4이면 3이 마지막열 
+    # n이 5이며 4가 마지막역
+    if n%2 != i%2:
         for j in range(n-1, -1, -1):
             arr[j][i] =num #[3][3] [2][3] [1][3] [0][3] / [0][2] [1][2] [2][2] [3][2]
+            num += 1
+
+    else:
+        for j in range(n):
+            arr[j][i] = num
             num += 1
 
 for row in arr:
