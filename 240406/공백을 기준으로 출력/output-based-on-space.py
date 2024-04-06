7f4f8a900e759b4dc41e7d5d69b4851c396d284a
hyeseon <46919444+HyeseonLee@users.noreply.github.com>
2024-04-06 19:10:42 +0900

    Title: 공백을 기준으로 출력, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240406/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+for elem in a:
+    if elem==" ":
+        pass
+    else:
+        print(elem, end="")
+for elem in b:
+    if elem==" ":
+        pass
+    else:      
+        print(elem, end="")
